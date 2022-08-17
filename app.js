9fb0eac553aa7397b25769154162b7ca7b0385bc
web-vikas <msg_vikas@yahoo.com>
2022-08-17 20:43:27 +0530

    Added Zero Before Hours if less then 10

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ window.addEventListener("DOMContentLoaded", () => {
   setInterval(() => {
     let dateTime = new Date();
     let hr = dateTime.getHours();
-    if (hr > 12) hr = hr - 12;
     let current_minutes = dateTime.getMinutes();
     let current_seconds = dateTime.getSeconds();
+    if (hr > 12) {
+      hr = hr - 12;
+    }
+    if (hr < 10) hr = "0" + hr;
+    if (current_seconds < 10) current_seconds = "0" + current_seconds;
+    if (current_minutes < 10) current_minutes = "0" + current_minutes;
     hrs.textContent = hr;
     minutes.textContent = current_minutes;
     seconds.textContent = current_seconds;
